0f8401ede1e84603f122d00c325e3b2f510e0c68
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-13 11:41:53 +0100

    add data folder creation

--- routes/pizzas.js
+++ routes/pizzas.js
@@ -1,38 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-const menu = [
-    {
-        id: 1,
-        name: "Margherita",
-        image: "imgs/margherita.webp",
-        ingredients: ["pomodoro", "mozzarella"],
-    },
-    {
-        id: 2,
-        name: "Marinara",
-        image: "imgs/marinara.jpeg",
-        ingredients: ["pomodoro", "aglio", "origano"],
-    },
-    {
-        id: 3,
-        name: "Diavola",
-        image: "imgs/diavola.jpeg",
-        ingredients: ["pomodoro", "mozzarella", "salame piccante"],
-    },
-    {
-        id: 4,
-        name: "Bufalina",
-        image: "imgs/bufalina.jpeg",
-        ingredients: ["pomodoro", "mozzarella di bufala"],
-    },
-    {
-        id: 5,
-        name: "4 formaggi",
-        image: "imgs/4_formaggi.jpeg",
-        ingredients: ["pomodoro", "mozzarella", "gorgonzola", "parmigiano", "ricotta"],
-    }
-]
+// import menu from Data
+
+const menu = require('../data/menu')
 
 /* CRUD OPERATIONS ON ENTITY PIZZA 
 - sostituisco app.get con router.get
