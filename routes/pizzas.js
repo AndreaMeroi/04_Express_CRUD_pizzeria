2506732cf5f6b79c0c3976c0cd25679ba50ca1af
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-13 16:45:32 +0100

    add destroy

--- routes/pizzas.js
+++ routes/pizzas.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // import menu from Data
 
-const menu = require('../data/menu')
+const menu = require('../data/pizzasMenu')
 
 /* CRUD OPERATIONS ON ENTITY PIZZA 
 - sostituisco app.get con router.get
@@ -14,10 +14,24 @@ const menu = require('../data/menu')
 // INDEX
 
 router.get('/', (req, res) => {
-    res.json(menu)
+
+    // res.send ('show a filtered list of pizzas')
+
+    console.log(req.query.ingredient);
+    // variabile filtered_menu che se non ci sono filtri attivi è uguale a menu stesso (uso let perchè dovrà poter essere modificata)
+
+    let filtered_menu = menu
+
+    if (req.query.ingredient) {
+        filtered_menu = menu.filter(pizza => pizza.ingredients.includes(req.query.ingredient))
+    }
+    console.log(filtered_menu);
+
+
+    res.json(filtered_menu)
 })
 
-//show
+//SHOW
 
 router.get('/:id', (req, res) => {
 
@@ -63,7 +77,34 @@ router.patch('/:id', (req, res) => {
 //destroy
 
 router.delete('/:id', (req, res) => {
-    res.send('Delete the single pizza with ID:' + req.params.id)
+
+    //seleziono la pizza con lo stesso codice di show
+
+    const { id } = req.params
+
+    console.log(id);
+
+    const pizza = menu.find(item => item.id === parseInt(id))
+    console.log(pizza);
+
+    if (!pizza) {
+
+        //concateno status(404) per modificare il codice i postman che altrimenti continuerebbe a proporre 200 OK
+
+        res.status(404).json({
+            error: true,
+            message: 'Resource not found'
+        })
+    }
+    /* utilizzo splice per RIMUOVERE la pizza selezionata (la selezione è fatta nello stesso modo di show)
+    dopo la virgola inico 1 perchè deve eliminare SOLO 1 elementoi*/
+
+    menu.splice(menu.indexOf(pizza), 1)
+    console.log(menu);
+
+    //se dovessi ripetere la richiesta di eliminare una pizza che già è stata eliminata sendStatus204 chiude la richesta
+    res.sendStatus(204)
+
 })
 
 module.exports = router
