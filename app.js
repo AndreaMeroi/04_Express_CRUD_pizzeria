66b527176bea94aafa799dc128a00b8683c04b14
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-17 20:46:52 +0100

    add middleware

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const app = express()
 const PORT = 3003
 const pizzasRouter = require('./routes/pizzas')
 const drinksRouter = require('./routes/drinks')
+//register logTime middleware (middleware global registration)
+const logTime = require('./middleware/logTime')
+//register serverError middleware (middleware global registration)
+const serverError = require('./middleware/serverError')
+//register notFound middleware (middleware global registration)
+const notFound = require('./middleware/notFound')
 
-//register static assets
+//register static assets (middleware global registration)
 app.use(express.static('public'))
-//register Body Parser for using store route
+//register Body Parser for using store route (middleware global registration)
 app.use(express.json())
+
+// SERVER LOGTIME MIDDLEWARE (MIDDLEWARE)
+
+app.use(logTime)
+
 //indico quale porta deve 'ascoltare' il server
 app.listen(PORT, () => {
 
@@ -37,3 +48,11 @@ app.use('/api/pizzas', pizzasRouter)
 // CRUD OPERATIONS ON ENTITY DRINKS
 
 app.use('/api/drinks', drinksRouter)
+
+// SERVER ERROR MIDDLEWARE (MIDDLEWARE)
+
+app.use(serverError)
+
+// SERVER NOTFOUND MIDDLEWARE (MIDDLEWARE)
+
+app.use(notFound)
